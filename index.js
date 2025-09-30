a3d53ba186621f6907fb593ea988cc8039dbf390
Gerson Barrientos <gersonbarrientos62@gmail.com>
2025-09-29 19:28:54 -0700

    index.js, worked on timestamps & validation

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+
+  // timestaps from the "age" elements
+  const timestamps = await page.$$eval(".age", (elements) => {
+    return elements.map((el) => {
+      // Hacker News uses title attribute for exact time
+      const raw = el.getAttribute("title");
+    });  
+  });
+
+  // take first 100
+  const first100 = timestamps.slice(0, 100);
+
+  // check count
+  if (first100.length != 100) {
+    console.error('Expected 100 articles, but found ${first100.length}');
+    await browser.close();
+    return;
+  }
+
+  // validate newest to olderst
+  const isSorted = first100.every((time, i) => i === 0 || time <= first100[i - 1]);
+
+  if (isSorted) {
+    console.error("Articles are not sorted from newest to oldest");
+  } else {
+    console.log("Exactly 100 articles sorted from newest to oldest");
+  }
+
+  await browser.close();
 }
 
 (async () => {
